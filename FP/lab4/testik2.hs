1e1ae62bd204e0879ec4988f0092e2ee51f1e091
Ilua <49995660+MurkMonk@users.noreply.github.com>
2019-04-26 13:27:20 +0300

    Added files

---
+++ FP/lab4/testik2.hs
@@ -0,0 +1,24 @@
+import Data.List
+import System.IO
+import System.Environment
+
+main = do
+    [filein, fileout] <- getArgs
+    handle <- openFile filein ReadMode
+    out <- openFile fileout WriteMode
+    contents <- hGetContents handle
+    let wrds = words contents
+    putStrLn "Введите слово, которое нужно заменить"
+    x <- getLine
+    putStrLn "Введите слово, на которое нужно заменить"
+    y <- getLine
+    hPutStr out $ replace x y wrds
+    hClose handle
+    hClose out
+
+replace :: String -> String -> [String] -> String
+replace x y [] = ""
+replace x y (s:str) = if (s == x) then
+             y ++ " " ++ (replace x y str)
+             else
+             s ++ " " ++ (replace x y str)
